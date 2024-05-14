7a984e18098e80956281338b25cadbee935f0383
sjungmin98 <shc980@gmail.com>
2024-05-14 05:29:05 +0000

    111

--- src/main/java/com/yojulab/study_springboot/controller/ConsultController.java
+++ src/main/java/com/yojulab/study_springboot/controller/ConsultController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.google.gson.Gson;
-import com.yojulab.study_springboot.service.sample.ApiService;
+import com.yojulab.study_springboot.service.ApiService;
 
 @Controller
 @RequestMapping("/consult")
--- src/main/java/com/yojulab/study_springboot/service/ApiService.java
+++ src/main/java/com/yojulab/study_springboot/service/ApiService.java
@@ -1,8 +1,9 @@
-package com.yojulab.study_springboot.service.sample;
+package com.yojulab.study_springboot.service;
 
 import java.util.Collections;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -15,16 +16,21 @@ public class ApiService {
     private RestTemplate restTemplate = new RestTemplate();
     private ObjectMapper mapper = new ObjectMapper();
 
+    @Value("${remote.restapi.url}")
+    private String baseUrl;
+
     public Map<String, Object> fetchDataConsume() {
-        String url = "http://localhost:8000/consult_api/Data_chart";
+        String url = baseUrl + "/consult_api/Data_chart";
         ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
         try {
             return mapper.readValue(response.getBody(), Map.class);
         } catch (JsonProcessingException e) {
-            // Log error and return empty list or rethrow as a custom checked/unchecked exception
+            e.printStackTrace();
+            return Collections.emptyMap();
+        } catch (Exception e) {
+            System.err.println("Error occurred while fetching data from: " + url);
             e.printStackTrace();
             return Collections.emptyMap();
         }
     }
-
 }
